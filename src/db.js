b8e907bb28d62791b770bc8189827344667f4a50
Anwesha <iamanwesha17@gmail.com>
2023-04-22 12:52:38 +0530

    connect to database

--- src/db.js
+++ src/db.js
@@ -1,1 +1,12 @@
 // connect to the database here
+const mongoose = require('mongoose');
+
+mongoose.set('strictQuery', false);
+const mongoURL = process.env.MONGO_URL;
+mongoose.connect(mongoURL);
+
+const database = mongoose.connection;
+database.on("error", (error) => console.log(error));
+database.once('connected', ()=> {
+    console.log('Database Connected')
+})
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const app = require('./app');
+require('./db');
 
 app.listen(8000, () => {
     console.log("server started at port 8000");
